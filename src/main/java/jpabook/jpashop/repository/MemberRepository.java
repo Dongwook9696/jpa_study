cb4d285f94dac0dbb09d8466dcbaa9f80e16d2f7
Dongwook9696 <59466899+Dongwook9696@users.noreply.github.com>
2021-02-15 20:11:11 +0900

    Member domain dev

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // (타입, PK)
+    }
+
+    public List<Member> findAll() { //        from의 대상은 엔티티
+         return em.createQuery("select m from Member", Member.class) // (jpql, 반환 타입(조회타입))
+                 .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
